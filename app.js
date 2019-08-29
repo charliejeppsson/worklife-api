76ad4c5282ec1a4577301d206151d2437d20e4b4
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2019-08-29 23:32:38 +0200

    Replace body-parser with built-in express parser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import logger from 'morgan'
-import bodyParser from 'body-parser'
 import apiRoutes from './routes/api/v1' 
 
 const app = express() // setup express application
@@ -10,9 +9,9 @@ const PORT = process.env.PORT || 5000
 // Log requests to the console
 app.use(logger('[:date[iso]] :method :url :status - :response-time ms'))
 
-// Parse incoming requests data
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+// Parse incoming requests data (bodyParser now included in express)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.use('/api/v1', apiRoutes)
