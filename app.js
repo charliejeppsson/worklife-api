b6f69708f7153779492a7e6b1ce615ad4023fb0e
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2019-09-13 00:22:29 +0200

    Remove REST-based logging and parsing Since we're now using graphql

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import cors from 'cors'
 import typeDefs from './schema';
 import resolvers from './resolvers';
 import db from './models';
-// import logger from 'morgan'
 
 const server = new ApolloServer({
   typeDefs: gql(typeDefs),
@@ -14,19 +13,13 @@ const server = new ApolloServer({
 
 // Initialize express app
 const app = express()
+
 // Integrate express app with apollo server middleware
 server.applyMiddleware({ app })
 
 // Declare dev env host and port
 const HOST = 'localhost'
 const PORT = process.env.PORT || 5000
-
-// Log requests to the console
-// app.use(logger('[:date[iso]] :method :url :status - :response-time ms'))
-
-// Parse incoming requests data (bodyParser now included in express)
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
 
 // Start listening for server requests;
 app.listen(PORT, HOST, () => {
